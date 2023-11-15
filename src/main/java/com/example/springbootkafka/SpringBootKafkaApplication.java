1f5bf43fa1aba1e25000b766d6aeda9e13e34652
parsaM110 <pmomeni80@gmail.com>
2023-11-15 22:39:27 +0330

    in docker you exec to container and under /bin you add this msg : kafka-console-consumer --topic amigoscode --from-beginning --bootstrap-server localhost:29092 / adding 1000 req in for loop

--- src/main/java/com/example/springbootkafka/SpringBootKafkaApplication.java
+++ src/main/java/com/example/springbootkafka/SpringBootKafkaApplication.java
@@ -15,7 +15,9 @@ public class SpringBootKafkaApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
         return args -> {
-            kafkaTemplate.send("amigoscode","hello kafka");
+            for (int i = 0 ; i < 10_000 ; i++) {
+                kafkaTemplate.send("amigoscode", "hello kafka :) " + i );
+            }
          };
     }
 
